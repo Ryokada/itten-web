ab817291472e29dc9addfb958c011e8cba6738a3
ryokada <ryokada.5088[gmail.com>
2023-09-21 20:33:20 +0900

    ヘッダーのアイコンをリンクに

--- itten-web-front/src/app/components/Header.tsx
+++ itten-web-front/src/app/components/Header.tsx
@@ -16,7 +16,9 @@ export default function Header() {
     return (
         <header>
             <div className='fixed top-0 left-0 flex w-full bg-white px-6 z-20 h-16 shadow-md items-center justify-between'>
-                <Image src={logo} alt='一天' className='h-10 w-10' />
+                <a href='/'>
+                    <Image src={logo} alt='一天' className='h-10 w-10' />
+                </a>
                 {isDesktop ? (
                     <DesktopLinks links={menuLinks} />
                 ) : (
