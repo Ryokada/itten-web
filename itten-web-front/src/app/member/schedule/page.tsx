0cb39606c440cf2981232f10115e0755c5ca4ed8
ryokada <ryokada.5088[gmail.com>
2023-09-25 02:30:00 +0900

    Firestoreからスケジュール一覧データを取得するところまで

--- itten-web-front/src/app/member/schedule/page.tsx
+++ itten-web-front/src/app/member/schedule/page.tsx
@@ -1,11 +1,22 @@
+import { CollectionReference } from 'firebase-admin/firestore';
 import type { Metadata } from 'next';
+import { dbAdmin } from '@/firebase/admin';
 
 export const metadata: Metadata = {
     title: '一天スケジュール',
     description: '一天メンバー用のスケジュールです',
 };
 
-const Schedule = () => {
+const Schedule = async () => {
+    const schedulesCollection = dbAdmin.collection('schedules') as CollectionReference<Schedule>;
+    const schedules = (
+        await schedulesCollection.orderBy('startTimestamp').startAt(new Date()).get()
+    ).docs;
+
+    console.log(
+        'schedules',
+        schedules.map((s) => s.data()),
+    );
     return (
         <main className='min-h-screen py-5 px-10'>
             <div className='max-w-xl w-full'>
